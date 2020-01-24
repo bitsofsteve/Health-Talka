287e6c1b224f0eca19cd4072fc7c95936c23cc95
cyberdroidmann <cyberlexix@gmail.com>
2020-01-24 16:06:20 +0100

    updated domain.yml file

--- actions.py
+++ actions.py
@@ -1,6 +1,7 @@
 from typing import Dict, Text, Any, List, Union, Optional
 
 from rasa_sdk import Tracker
+from rasa_sdk import action
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
 
